41db1daeb9596fe5315ea70825d0a3d07d67a087
praduman2714 <pradu14jsr@gmail.com>
2023-04-16 15:06:01 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const port = 8000;
+
+const app = express();
+
+
+app.listen(port, function(err){
+    if(err){
+        console.log("Error is " + err);
+        return ;
+    }
+
+    console.log('Server is up and running in port ' + port);
+})
