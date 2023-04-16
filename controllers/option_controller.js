1fdb02d088e8cdd80e268078e16f8d8ae5cd914e
praduman2714 <pradu14jsr@gmail.com>
2023-04-17 00:09:46 +0530

    Added the functionality of deleting the options

--- controllers/option_controller.js
+++ controllers/option_controller.js
@@ -13,7 +13,7 @@ module.exports.addOption = async function(req, res){
             })
             question.options.push(currOption.id);
             
-        }
+        } 
         question.save();
 
         return res.status(200).json({
@@ -25,5 +25,38 @@ module.exports.addOption = async function(req, res){
             message : 'Error in creating the option',
             error : err.message
         })
+    }
+}
+
+
+module.exports.deleteOptions = async function(req, res){
+    try{
+        const option = await Option.findById(req.params.id);
+
+        // finding the question and updating it
+        await Question.updateOne(
+            { options: { $in: req.params.id }},
+            { $pull: { options: { $eq: req.params.id }}}
+        );
+
+        await Option.deleteOne({_id : option });
+
+        return res.status(200).json({
+            message : 'Option Deleted Successfully'
+        })
+    }catch(err){
+        return res.status(404).json({
+            message : 'Error in deleting the option',
+            error : err.message
+        })
+    }
+}
+
+
+module.exports.addVotes = async function(req, res){
+    try{
+
+    }catch(err){
+        
     }
 }
--- routes/option.js
+++ routes/option.js
@@ -1,4 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const optionController = require('../controllers/option_controller');
+
+router.get('/:id/delete' , optionController.deleteOptions);
+router.get('/:id/addVotes', optionController.addOption);
 
 module.exports = router;
